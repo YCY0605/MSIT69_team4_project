2fcf26ac01a8a949196c93665e8201ea8db10f30
YCY0605 <u6ru3ul6@gmail.com>
2026-01-06 16:36:28 +0800

    進度回報

---
+++ MSIT69_team4_project/Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSIT69_team4_project.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Progress()
+        {
+            return View();
+        }
+    }
+}
